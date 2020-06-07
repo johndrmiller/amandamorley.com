a7392100151f4fe7b7d490d629456af2e89b589e
johndrmiller <johndrmiller@gmail.com>
2020-06-07 17:48:08 -0400

    Version 0.8 Updated Copyright year. Deleted outdated comments from HTML pages. Added potential fix for pages not fitting correctly in mobile browsers.

---
+++ js/mobileheight.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("orientationchange", setWindowHeight);
+
+function setWindowHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setWindowHeight();
